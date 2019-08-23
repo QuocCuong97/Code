8b972c2cd818b7639c68414e033d6541d8b62426
Ngo Quoc Cuong <49702138+QuocCuong97@users.noreply.github.com>
2019-08-24 02:07:04 +0700

    02:07

--- ssh.sh
+++ ssh.sh
@@ -2,15 +2,62 @@
 ## Secure SSH on CentOS 7
 DIRECTORY=$(cd `dirname $0` && pwd)
 change_port_number(){
-    echo -n "Do you want to change default port (22) for SSH? (Y/n)"
-    read ans1
-    if [ $ans1 eq Y ] 
+    echo -n "Change default port (22) for SSH? [Y/n] "
+    read ans
+    if [ "$ans" = "Y" ] 
     then
-        echo -n "Which port do you want?"
+        echo -n "Which port do you want? "
         read port
-        sed -i -e "s/"\#Port 22"/"Port "$port""/g" /etc/ssh/sshd_config
+        sed -i -e "s/\#Port 22/Port $port/g" /etc/ssh/sshd_config
     else
-        echo -n "Nothing changed!"
+        echo -n "Nothing changed!\n"
     fi
 }
-change_port_number()
+disable_root_login(){
+    echo -n "Disable root login? [Y/n] "
+    read ans
+    if [ "$ans" = "Y" ] 
+    then
+    sed -i -e "s/\#PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
+    else
+        echo -n "Nothing changed! \n"
+    fi
+}
+max_auth_tries(){
+    echo -n "Maximum authentication attempts : "
+    read ans
+    if [ "$ans" = "" ] 
+    then
+        echo -n "Nothing changed! \n"
+    else
+        sed -i -e "s/\#MaxAuthTries 6/MaxAuthTries $ans/g" /etc/ssh/sshd_config
+    fi
+}
+max_sessions(){
+    echo -n "Maximum SSH sessions : "
+    read ans
+    if [ "$ans" = "" ] 
+    then
+        echo -n "Nothing changed! \n"
+    else
+        sed -i -e "s/\#MaxSessions 10/MaxSessions $ans/g" /etc/ssh/sshd_config
+    fi    
+}
+disable_password(){
+    echo -n "Disable password-based authentication (Public-Key instead)? [Y/n] "
+    read ans
+    if [ "$ans" = "Y" ] 
+    then
+    sed -i -e "s/\#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
+    sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
+    else
+        echo -n "Nothing changed! \n"
+    fi
+}
+change_port_number
+disable_root_login
+disable_password
+max_auth_tries
+max_sessions
+systemctl restart sshd
+
