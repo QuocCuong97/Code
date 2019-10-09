3ad98d97ffe2a967a9edb4e2bc3f1ac617a8999f
Ngo Quoc Cuong <49702138+QuocCuong97@users.noreply.github.com>
2019-10-10 01:48:36 +0700

    01:48

--- Code/Python/wordpress/wordpress.py
+++ Code/Python/wordpress/wordpress.py
@@ -1,14 +1,16 @@
 import re
 import os
+import json
 import subprocess
+# Function create Database
 def creat_database():
-    mariahost = input('MariaDB Host (localhost): ')
-    if mariahost == '':
-        mariahost == 'localhost'
-    mariadb = input('New MariaDB Name: ')
-    mariauser = input('New MariaDB User: ')
-    mariapass = input('Password: ')
-
+    #op = open('settings.json')
+    #rd = op.read()
+    #data = json.dumps(rd)
+    #print(data['mariahost'])
+    global(mariadb) = "wordpress"
+    global(mariauser) = "wordpressuser"
+    global(mariapass) = "P@ssw0rd"
 # Function install some PHP packages
 def install_php():
     os.system("yum install -y php-gd php-mysql")
@@ -28,8 +30,9 @@ def config_wordpress():
     os.system("chown -R apache:apache /var/www/html/")
     os.system("cd /var/www/html/")
     os.system("cp wp-config-sample.php wp-config.php")
-    os.system("sed -i -e "s/database_name_here/$mariadb/g" wp-config.php")
+    os.system("sed -i -e "s/database_name_here/"$mariadb"/g" wp-config.php")
     os.system("sed -i -e "s/username_here/"$mariauser"/g" wp-config.php")
     os.system("sed -i -e "s/password_here/"$mariapass"/g" wp-config.php")
     # Tidy up
-    
+    os.system("rm -Rf /tmp/wordpress")
+    os.system("rm -f /tmp/latest.tar.gz")
