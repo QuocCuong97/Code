52416d08e20f4a66899e7dfc17fa7e163fe3b8b8
QuocCuong97 <cuongnq24101997@gmail.com>
2020-02-16 18:13:06 +0700

    18:13

---
+++ Pricelist/home/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+    homepage = models.TextField()
+
+class Domain(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    domain_type_choices = [
+        ('vn', '.vn'),
+        ('com', '.com'),
+        ('comvn', '.com.vn'),
+        ('net', '.net'),
+        ('org', '.org'),
+        ('info', '.info'),
+    ]
+    domain_type = models.CharField(
+        max_length=10,
+        choices=domain_type_choices,
+        default='vn',
+    )
+    origin_price = models.CharField(max_length=20, default="0")
+    sale_price = models.CharField(max_length=20, default="0")
